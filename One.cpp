9cfe3208837f0e54006463e17ce7ad6bd30754ac
Amit Joshi <aj37171@gmail.com>
2021-07-31 15:52:37 +0530

    Sum to Sum in 1.cpp

--- One.cpp
+++ One.cpp
@@ -5,7 +5,7 @@ int main()
     int a,b,c;
     cout<<"Enter any Two Number =";
     cin>>a>>b;
-    c=a+b;
-    cout<<"Sum of Two Number ="<<c;
+    c=a-b;
+    cout<<"Sub of Two Number ="<<c;
     return 0;
 }
